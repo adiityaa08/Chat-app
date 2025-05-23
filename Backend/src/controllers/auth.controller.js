9353860c60a06dcc9ebda6d3973168dd0b6ee521
adityasathawane <adityasathawane2003@gmail.com>
2025-05-23 17:22:40 +0530

    Bug fixed

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -89,25 +89,28 @@ export const logout=(req,res)=>{
     }
 }
 
-export const updateProfile=async(req,res)=>{
+export const updateProfile = async (req, res) => {
   try {
-    const {profilePic}=req.body;
-    const userId=req.user._id;
-    
-    if(!profilePic)
-    {
-        return res.status(400).json({message:"Profile pic is required"});
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+
+    if (!profilePic) {
+      return res.status(400).json({ message: "Profile pic is required" });
     }
-    const uploadresponse=await cloudinary.uploader.upload(profilePic);
-    const updateduser=User.findByIdAndUpdate(userId,{profilePic:uploadresponse.secure_url},{new:true});
 
-    res.status(200).json(updateduser);
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
 
-   } catch (error) {
+    res.status(200).json(updatedUser);
+  } catch (error) {
     console.log("error in update profile:", error);
     res.status(500).json({ message: "Internal server error" });
   }
-}
+};
 
 export const checkAuth = (req, res) => {
     try {
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -14,7 +14,9 @@ const app=express();
 const PORT=process.env.PORT
 
 
-app.use(express.json())
+app.use(express.json({ limit: '10mb' }));
+// app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
 app.use(cookieParser())
 app.use(cors({
     origin:"http://localhost:5173",
@@ -23,7 +25,7 @@ app.use(cors({
 );
 
 app.use("/api/auth",authRoutes)
-app.use("api/message",messageRoutes);
+app.use("/api/message",messageRoutes);
 
 
 app.listen(PORT,()=>{

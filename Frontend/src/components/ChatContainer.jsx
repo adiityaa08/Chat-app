d4e3f97c5050fa2ba0bfcdf2ab499767bfaa1889
adityasathawane <adityasathawane2003@gmail.com>
2025-05-25 15:04:57 +0530

    Refactor message rendering in ChatContainer component

--- Frontend/src/components/ChatContainer.jsx
+++ Frontend/src/components/ChatContainer.jsx
@@ -30,46 +30,41 @@ const ChatContainer = () => {
         <ChatHeader/>
 
         <div className="flex-1 overflow-y-auto p-4 space-y-4">
-        {messages.map((message) => {
-  console.log("Message:", message);
-  console.log("Sender ID:", message.senderId, "Auth User ID:", authUser._id);
-
-  return (
-    <div
-      key={message._id}
-      className={`chat ${String(message.senderId) === String(authUser._id) ? "chat-end" : "chat-start"}`}
-    >
-      <div className="chat-image avatar">
-        <div className="size-10 rounded-full border">
-          <img
-            src={
-              String(message.senderId) === String(authUser._id)
-                ? authUser.profilePic || "/avatar.png"
-                : selectedUser.profilePic || "/avatar.png"
-            }
-            alt="profile pic"
-          />
-        </div>
-      </div>
-      <div className="chat-header mb-1">
-        <time className="text-xs opacity-50 ml-1">
-          {formatMessageTime(message.createdAt)}
-        </time>
-      </div>
-      <div className="chat-bubble flex flex-col">
-        {message.image && (
-          <img
-            src={message.image}
-            alt="Attachment"
-            className="sm:max-w-[200px] rounded-md mb-2"
-          />
-        )}
-        {message.text && <p>{message.text}</p>}
-      </div>
-    </div>
-  );
-})}
-
+        {messages.map((message) => (
+          <div
+            key={message._id}
+            className={`chat ${String(message.senderId) === String(authUser._id) ? "chat-end" : "chat-start"}`}
+          >
+            
+            <div className=" chat-image avatar">
+              <div className="size-10 rounded-full border">
+                <img
+                  src={
+                    message.senderId === authUser._id
+                      ? authUser.profilePic || "/avatar.png"
+                      : selectedUser.profilePic || "/avatar.png"
+                  }
+                  alt="profile pic"
+                />
+              </div>
+            </div>
+            <div className="chat-header mb-1">
+              <time className="text-xs opacity-50 ml-1">
+                {formatMessageTime(message.createdAt)}
+              </time>
+            </div>
+            <div className="chat-bubble flex flex-col">
+              {message.image && (
+                <img
+                  src={message.image}
+                  alt="Attachment"
+                  className="sm:max-w-[200px] rounded-md mb-2"
+                />
+              )}
+              {message.text && <p>{message.text}</p>}
+            </div>
+          </div>
+        ))}
       </div>
 
         <MessageInput/>

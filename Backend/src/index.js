a2df48fc3b7733470f1ccefdf7acf96b6f3cb8e5
adityasathawane <adityasathawane2003@gmail.com>
2025-08-15 23:05:58 +0530

    changed cors policy 2

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -23,7 +23,7 @@ app.use(express.json({ limit: '10mb' }));
 
 app.use(cookieParser())
 app.use(cors({
-    origin:"*",
+    origin:"https://nex-chat-frontend.onrender.com",
     credentials:true,
 })
 );

7306f870fca36f390bc92ee663906670746056d0
adityasathawane <adityasathawane2003@gmail.com>
2025-08-15 23:24:24 +0530

    Changed cors policy

--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from '../lib/axios.js';
 import { toast } from 'react-hot-toast';
 import { io } from "socket.io-client";
 
-const BASE_URL=import.meta.env.MODE === "development"?"http://localhost:3000":"/"; // Update with your backend URL
+const BASE_URL=import.meta.env.MODE === "development"?"http://localhost:3000":"https://nex-chat-frontend.onrender.com"; // Update with your backend URL
 export const useAuthStore = create((set,get)=>({
     authUser: null,
     isSigningUp: false,

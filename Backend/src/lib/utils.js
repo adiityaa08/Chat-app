9fc2e66093a1568ff4dec6c603eddace8818c5b2
adityasathawane <adityasathawane2003@gmail.com>
2025-08-16 18:43:54 +0530

    Updated the backend for token

--- Backend/src/lib/utils.js
+++ Backend/src/lib/utils.js
@@ -1,15 +1,16 @@
 import jwt from "jsonwebtoken";
-export const generateToken = (userId,res)=>{
 
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-        expiresIn: "7d",
-      });
-    res.cookie("jwt",token,{
-        maxAge:7*24*60*60*1000,  // 7days
-        httpOnly:true,    // prevent client side js from accessing the cookie
-        sameSite:"strict", //prevent CSRF attack
-        secure:process.env.NODE_ENV!=="development" // only send cookie over https
-    });
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
 
-    return token;
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // only https in production
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
+
+  return token;
 };
